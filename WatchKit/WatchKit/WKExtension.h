a0d00486bc4558e627d324ddf34e9ddf27f78fd8
David Post <david.post@replicon.com>
2026-06-03 18:06:06 -0600

    update

--- WatchKit/WatchKit/WKExtension.h
+++ WatchKit/WatchKit/WKExtension.h
@@ -28,13 +28,22 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)applicationWillResignActive;
 
 - (void)handleActionWithIdentifier:(nullable NSString *)identifier forRemoteNotification:(NSDictionary *)remoteNotification; // when the app is launched from a notification. If launched from app icon in notification UI, identifier will be empty
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 - (void)handleActionWithIdentifier:(nullable NSString *)identifier forLocalNotification:(UILocalNotification *)localNotification; // when the app is launched from a notification. If launched from app icon in notification UI, identifier will be empty
+#pragma clang diagnostic pop
 - (void)handleActionWithIdentifier:(nullable NSString *)identifier forRemoteNotification:(NSDictionary *)remoteNotification withResponseInfo:(NSDictionary *)responseInfo; // when the app is launched from a notification. If launched from app icon in notification UI, identifier will be empty
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 - (void)handleActionWithIdentifier:(nullable NSString *)identifier forLocalNotification:(UILocalNotification *)localNotification withResponseInfo:(NSDictionary *)responseInfo; // when the app is launched from a notification. If launched from app icon in notification UI, identifier will be empty
+#pragma clang diagnostic pop
 - (void)handleUserActivity:(nullable NSDictionary *)userInfo;
 
 - (void)didReceiveRemoteNotification:(NSDictionary *)userInfo;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 - (void)didReceiveLocalNotification:(UILocalNotification *)notification;
+#pragma clang diagnostic pop
 
 @end
 
